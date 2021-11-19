434df1d893fb455644268f2d733055e9f7c5c44e
wallacemachado <wallacefm18@gmail.com>
2021-11-19 20:08:30 -0300

    ajustes em  upload de 1 objeto

--- application/services/upload_manager.go
+++ application/services/upload_manager.go
@@ -3,10 +3,7 @@ package services
 import (
 	"context"
 	"io"
-	"log"
 	"os"
-	"path/filepath"
-	"runtime"
 	"strings"
 
 	"cloud.google.com/go/storage"
@@ -56,84 +53,4 @@ func (vu *VideoUpload) UploadObject(objectPath string, client *storage.Client, c
 
 	return nil
 
-}
-
-func (vu *VideoUpload) loadPaths() error {
-
-	err := filepath.Walk(vu.VideoPath, func(path string, info os.FileInfo, err error) error {
-
-		if !info.IsDir() {
-			vu.Paths = append(vu.Paths, path)
-		}
-		return nil
-	})
-
-	if err != nil {
-		return err
-	}
-	return nil
-}
-
-func (vu *VideoUpload) ProcessUpload(concurrency int, doneUpload chan string) error {
-
-	in := make(chan int, runtime.NumCPU()) // qual o arquivo baseado na posicao do slice Paths
-	returnChannel := make(chan string)
-
-	err := vu.loadPaths()
-	if err != nil {
-		return err
-	}
-
-	uploadClient, ctx, err := getClientUpload()
-	if err != nil {
-		return err
-	}
-
-	for process := 0; process < concurrency; process++ {
-		go vu.uploadWorker(in, returnChannel, uploadClient, ctx)
-	}
-
-	go func() {
-		for x := 0; x < len(vu.Paths); x++ {
-			in <- x
-		}
-		close(in)
-	}()
-
-	for r := range returnChannel {
-		if r != "" {
-			doneUpload <- r
-			break
-		}
-	}
-
-	return nil
-
-}
-
-func (vu *VideoUpload) uploadWorker(in chan int, returnChan chan string, uploadClient *storage.Client, ctx context.Context) {
-
-	for x := range in {
-		err := vu.UploadObject(vu.Paths[x], uploadClient, ctx)
-
-		if err != nil {
-			vu.Errors = append(vu.Errors, vu.Paths[x])
-			log.Printf("error during the upload: %v. Error: %v", vu.Paths[x], err)
-			returnChan <- err.Error()
-		}
-
-		returnChan <- ""
-	}
-
-	returnChan <- "upload completed"
-}
-
-func getClientUpload() (*storage.Client, context.Context, error) {
-	ctx := context.Background()
-
-	client, err := storage.NewClient(ctx)
-	if err != nil {
-		return nil, nil, err
-	}
-	return client, ctx, nil
 }
