adc0edb1677a9abb30d9b826e26d5934126bd7fa
wallacemachado <wallacefm18@gmail.com>
2021-11-19 14:31:19 -0300

    fragmentando video com BENTO4

--- application/services/video_services.go
+++ application/services/video_services.go
@@ -5,6 +5,7 @@ import (
 	"io/ioutil"
 	"log"
 	"os"
+	"os/exec"
 
 	"cloud.google.com/go/storage"
 	"github.com/wallacemachado/microsservico-encoder-go/application/repositories"
@@ -59,4 +60,33 @@ func (v *VideoService) Download(bucketName string) error {
 	log.Printf("video %v has been stored", v.Video.ID)
 
 	return nil
+}
+
+func (v *VideoService) Fragment() error {
+
+	// od.ModePerm dá permissão na pasta
+	err := os.Mkdir(os.Getenv("localStoragePath")+"/"+v.Video.ID, os.ModePerm)
+	if err != nil {
+		return err
+	}
+
+	source := os.Getenv("localStoragePath") + "/" + v.Video.ID + ".mp4"
+	target := os.Getenv("localStoragePath") + "/" + v.Video.ID + ".frag"
+
+	//mp4 foi instalado pelo dockerfile
+	cmd := exec.Command("mp4fragment", source, target)
+	output, err := cmd.CombinedOutput()
+	if err != nil {
+		return err
+	}
+
+	printOutput(output)
+
+	return nil
+}
+
+func printOutput(out []byte) {
+	if len(out) > 0 {
+		log.Printf("=====> Output: %s\n", string(out))
+	}
 }
